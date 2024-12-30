ce359132b3397f1f7aab1be9e6749a5f767065da
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2024-12-30 09:34:43 +0600

    Added Axios Interceptor feature

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,32 @@
 import axios from "axios";
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 const useAxiosSecure = () => {
+  const { logOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (error) => {
+        if (error.status === 401 || error.status === 403) {
+          logOut()
+            .then(() => {
+              navigate("/login");
+            })
+            .catch((err) => console.log(err));
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
   return axiosInstance;
 };
 
